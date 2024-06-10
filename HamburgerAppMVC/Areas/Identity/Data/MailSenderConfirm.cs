ee95cff9f56cf971622c7cdf09a78f631c194898
smyy.cskn@hotmail.com <Sumeyye12A>
2024-06-10 10:59:05 +0300

    Mail Confirm eklendi.

---
+++ HamburgerAppMVC/Areas/Identity/Data/MailSenderConfirm.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace HamburgerAppMVC.Areas.Identity.Data
+{
+    public class MailSenderConfirm : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                SmtpClient smtpClient = new SmtpClient();
+
+                var sender = "sepetsepethamburger@gmail.com";
+
+                mailMessage.From = new MailAddress(sender);//Kimden 
+                mailMessage.To.Add(email);//Kime
+                mailMessage.Subject = subject;
+                mailMessage.Body = htmlMessage;
+
+                smtpClient.Port = 587;
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Credentials = new NetworkCredential(sender, "rmfyqrblxuxvmkzt");
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                smtpClient.Send(mailMessage);
+
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException(ex);
+            }
+        }
+    }
+}

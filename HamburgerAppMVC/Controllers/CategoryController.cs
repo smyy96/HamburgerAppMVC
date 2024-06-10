e1307d6956f7ead3bd490ddc2e4365d1b1df78e4
smyy.cskn@hotmail.com <Sumeyye12A>
2024-06-10 23:22:26 +0300

    Crud işlemlerden Category Listeleme yapıldı.

---
+++ HamburgerAppMVC/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using HamburgerAppMVC.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HamburgerAppMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}

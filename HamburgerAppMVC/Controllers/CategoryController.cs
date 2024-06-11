1da82fd128e1bb2fcbef1ecc03fccf364fb9a1fb
smyy.cskn@hotmail.com <Sumeyye12A>
2024-06-11 15:45:46 +0300

    Category silme eklendi.

--- HamburgerAppMVC/Controllers/CategoryController.cs
+++ HamburgerAppMVC/Controllers/CategoryController.cs
@@ -19,10 +19,10 @@ namespace HamburgerAppMVC.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        
+
         public async Task<IActionResult> Details(int id)
         {
-            
+
             var category = await _context.Categories
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (category == null)
@@ -34,74 +34,41 @@ namespace HamburgerAppMVC.Controllers
         }
 
 
+
         [HttpPost]
-        public async Task<IActionResult> Edit(int id, Category category)
+        public async Task<IActionResult> Delete(int id)
         {
-            if (id != category.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(category);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExists(category.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
-        
-
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
             var category = await _context.Categories
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
 
-            return View(category);
-        }
 
-        // POST: Categories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var category = await _context.Categories.FindAsync(id);
-            if (category != null)
-            {
-                _context.Categories.Remove(category);
-            }
-
+            category.IsActive = false;
+            _context.Update(category);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return Json(new { success = true });
         }
 
-        private bool CategoryExists(int id)
-        {
-            return _context.Categories.Any(e => e.Id == id);
-        }
+
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> Edit(int id, Category category)
+        //{
+        //    if (id != category.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(category);
+        //}
+
+
     }
 }

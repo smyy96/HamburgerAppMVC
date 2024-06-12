07bedb9286c2ff9b44b31efcd0a08a8f803a3923
Eyşan Tekinsoy <eysantekinsoy@hotmail.com>
2024-06-12 14:18:39 +0300

    User Page projeye eklendi

--- HamburgerAppMVC/Program.cs
+++ HamburgerAppMVC/Program.cs
@@ -55,7 +55,7 @@ namespace HamburgerAppMVC
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=UserPage}/{action=Index}/{id?}");
 
             app.Run();
         }
